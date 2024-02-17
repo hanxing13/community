92d600f9a67e34ed10d4ae174e173ad991ca8061
hanxing13 <1265788270@qq.com>
2024-02-17 22:06:39 +0800

    增加功能，显示登录信息

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -163,4 +163,7 @@ public class UserService implements CommunityConstant {
         loginTicketMapper.updateStatus(ticket,1);
     }
 
+    public LoginTicket findLoginTicket(String ticket){
+        return loginTicketMapper.selectByTicket(ticket);
+    }
 }
