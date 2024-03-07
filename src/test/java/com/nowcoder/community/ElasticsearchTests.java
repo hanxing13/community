eff54e9dd442425c8c635b67010e5ca04e16d0cf
hanxing <1265788270@qq.com>
2024-03-08 00:08:24 +0800

    es搜索功能放弃，以后再说

---
+++ src/test/java/com/nowcoder/community/ElasticsearchTests.java
@@ -0,0 +1,88 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.elasticsearch.DiscussPostRepository;
+import com.nowcoder.community.entity.DiscussPost;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortBuilders;
+import org.elasticsearch.search.sort.SortOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQuery;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class ElasticsearchTests {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private DiscussPostRepository discussRepository;
+
+    @Autowired
+    private ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @Test
+    public void testInsert(){
+        discussRepository.save(discussPostMapper.selectDiscussPostById(241));
+        discussRepository.save(discussPostMapper.selectDiscussPostById(242));
+        discussRepository.save(discussPostMapper.selectDiscussPostById(243));
+    }
+
+    @Test
+    public void testInsertList(){
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(101, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(102, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(103, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(111, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(112, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(131, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(132, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(133, 0 ,100));
+        discussRepository.saveAll(discussPostMapper.selectDiscussPosts(134, 0 ,100));
+    }
+
+    @Test
+    public void testUpdate(){
+        DiscussPost post = discussPostMapper.selectDiscussPostById(231);
+        post.setContent("我是新人，使劲灌水。");
+        discussRepository.save(post);
+    }
+
+    @Test
+    public void testDelete(){
+//        discussPostRepository.deleteById(231);
+        discussRepository.deleteAll();
+    }
+
+//    @Test
+//    public void testSearchByRepository(){
+//        NativeSearchQuery searchQuery = new NativeSearchQueryBuilder()
+//                .withQuery(QueryBuilders.multiMatchQuery("互联网寒冬", "title", "content"))
+//                .withSort(SortBuilders.fieldSort("type").order(SortOrder.DESC))
+//                .withSort(SortBuilders.fieldSort("score").order(SortOrder.DESC))
+//                .withSort(SortBuilders.fieldSort("createTime").order(SortOrder.DESC))
+//                .withPageable(PageRequest.of(0, 10))
+//                .withHighlightFields(
+//                        new HighlightBuilder.Field("title").preTags("<em>").postTags("</em>"),
+//                        new HighlightBuilder.Field("content").preTags("<em>").postTags("</em>")
+//                ).build();
+//        Page<DiscussPost> page = discussRepository.searchSimilar(searchQuery);
+//        System.out.println(page.getTotalElements());
+//        System.out.println(page.getTotalPages());
+//        System.out.println(page.getNumber());
+//        System.out.println(page.getSize());
+//        for (DiscussPost post : page){
+//            System.out.println(post);
+//        }
+//    }
+
+}
